4f49b1cbade51b8b754b64f0f1c500037b9ec8a0
Ashok Kumar Pant <ashokpant87@gmail.com>
2022-08-01 16:58:22 +0545

    Add example

--- examples/yolov7_example.py
+++ examples/yolov7_example.py
@@ -1,4 +1,17 @@
+import cv2
+
 from yolov7.detector import YoloV7Detector
 
 if __name__ == '__main__':
-    yolo = YoloV7Detector()
+    yolo = YoloV7Detector("yolov7.pt")
+    images = ["inference/images/bus.jpg", "inference/images/horses.jpg", "inference/images/image1.jpg",
+              "inference/images/image2.jpg"]
+    for image in images:
+        img = cv2.imread(image)
+        boxes, class_ids, confidences = yolo.detect(img)
+        for box, cls_id, conf in zip(boxes, class_ids, confidences):
+            cv2.rectangle(img, (box[0], box[1]), (box[2], box[3]), (0, 255, 0), 2)
+            text = str(yolo.labels[cls_id]) + ": {:.2f}".format(conf)
+            cv2.putText(img, text, (box[0], box[1]), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+        cv2.imshow("image", img)
+        cv2.waitKey(0)
--- yolov7/detector.py
+++ yolov7/detector.py
@@ -1,4 +1,3 @@
-import cv2
 import numpy as np
 import torch
 from numpy import random
@@ -57,18 +56,3 @@ class YoloV7Detector:
                 confidences.append(float(conf))
                 class_ids.append(int(cls))
         return boxes, class_ids, confidences
-
-
-if __name__ == '__main__':
-    yolo = YoloV7Detector("yolov7.pt")
-    images = ["inference/images/bus.jpg", "inference/images/horses.jpg", "inference/images/image1.jpg",
-              "inference/images/image2.jpg"]
-    for image in images:
-        img = cv2.imread(image)
-        boxes, class_ids, confidences = yolo.detect(img)
-        for box, cls_id, conf in zip(boxes, class_ids, confidences):
-            cv2.rectangle(img, (box[0], box[1]), (box[0] + box[3], box[1] + box[2]), (0, 255, 0), 2)
-            text = str(yolo.labels[cls_id]) + ": {:.2f}".format(conf)
-            cv2.putText(img, text, (box[0], box[1]), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
-        cv2.imshow("image", img)
-        cv2.waitKey(0)
